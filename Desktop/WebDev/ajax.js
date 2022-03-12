5d7a90a26707762973f7a9febc00ac3bdeeb6196
MacLaren Scott <101377119+maclarenscott@users.noreply.github.com>
2022-03-12 17:28:30 -0500

    fixing things

--- Desktop/WebDev/ajax.js
+++
@@ -1,3 +0,0 @@
-const h1 = document.querySelector("h1");
-const obj = JSON.parse('{"name":"mike","age":12}');
-h1.innerText = obj.name;
--- Desktop/WebDev/node.js
+++
@@ -1,4 +0,0 @@
-const args = process.argv.slice(2);
-for(let a of args){
-    console.log(a);
-}
